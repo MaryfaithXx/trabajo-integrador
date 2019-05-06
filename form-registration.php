df874ba91f2609ba50320d1b5ae76d436f9e87e8
Mayra1988 <mayra.soruco@hotmail.com>
2019-05-06 12:35:34 -0300

    actualizacion de registro en php

--- form-registration.php
+++ form-registration.php
@@ -8,12 +8,13 @@
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
   </head>
-	<body>
-		<!-- main-header -->
-		<?php
-			require_once("partials/nav-bar.php");
-		?>
-		<!-- /main-header -->
+  <body>
+    <!-- main-header -->
+    <?php
+		  require_once("partials/nav-bar.php");
+	   ?>
+    <!-- /main-header -->
+
 
 		<!-- formulario -->
     <div class="fondo">
@@ -60,10 +61,12 @@
     </div>
 
     <!-- formulario -->
+
     <!-- main footer -->
-		<?php
+    <?php
 			require_once("partials/footer.php");
 		?>
 		<!-- /main footer -->
-	</body>
+
+  </body>
 </html>
--- partials/nav-bar.php
+++ partials/nav-bar.php
@@ -6,7 +6,7 @@
 			<a href="wander_index.html"><img class="logo" src="images/wander-logo.png" alt="wander logotype"/></a>
 		</div>
 		<div class="col-2">
-			<a href="form-registration.html" class="register-link">Registrarme</a>
+			<a href="form-registration.php" class="register-link">Registrarme</a>
 		</div>
 		<div class="col-2">
 			<a href="form-login.html" class="login-button">Inicio</a>
