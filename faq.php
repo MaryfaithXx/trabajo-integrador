a68b925276b7ca75930bdefc60d2b64e71d6bb37
Yemina Maricel Queirolo <yeminaqueirolo@gmail.com>
2019-05-07 08:12:28 -0300

    Nuevo faq.php y faq.scc

---
+++ faq.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Wander - Conectando viajeros en el mundo</title>
+		<link rel="stylesheet" href="css/faq.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+  		<!-- main-header -->
+      <header class="main-header">
+      	<div class="container-fluid">
+      	<div class="row">
+      		<div class="col-8">
+      			<img class="iso" src="images/wander-iso.png" alt="wander isotype"/>
+      			<a href="wander_index.html"><img class="logo" src="images/wander-logo.png" alt="wander logotype"/></a>
+      		</div>
+      		<div class="col-2">
+      			<a href="form-registration.html" class="register-link">Registrarme</a>
+      		</div>
+      		<div class="col-2">
+      			<a href="form-login.html" class="login-button">Inicio</a>
+      		</div>
+      	</div>
+      	</div>
+      </header>
+      <!-- /accordion -->
+      <div class="container">
+  <h2>Preguntas Frecuentes</h2>
+  <h3>En esta seccion encontraras todo lo necesario para que tu única preocupación en Wander sea tu próximo destino....</h3>
+  <div id="accordion">
+    <div class="card">
+      <div class="card-header">
+        <a class="card-link" data-toggle="collapse" href="#collapseOne">
+          Hacerse Miembro - ¿Es gratis Wander?
+        </a>
+      </div>
+      <div id="collapseOne" class="collapse show" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
+        Perfil - ¿Como edito mis datos de la cuenta?
+      </a>
+      </div>
+      <div id="collapseTwo" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
+          Perfil - ¿Como elimino la cuenta?
+        </a>
+      </div>
+      <div id="collapseThree" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">
+          Gestion de Cuenta - ¿Como creo mi Usuario y Contraseña?
+        </a>
+      </div>
+      <div id="collapseFour" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseFive">
+          Gestion de Cuenta - ¿Como recupero mi Usuario y Contraseña?
+        </a>
+      </div>
+      <div id="collapseFive" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseSix">
+          Privacidad - ¿como envio una solicitud de amistad?
+        </a>
+      </div>
+      <div id="collapseSix" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseSeven">
+          Privacidad - ¿Como Bloqueo y Desbloqueo Amigos?
+        </a>
+      </div>
+      <div id="collapseSeven" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseEight">
+          Comunidad - ¿Como sumo eventos a mi TimeLine?
+        </a>
+      </div>
+      <div id="collapseEight" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+      </div>
+    </div>
+    </div>
+</div>
+<!-- /accordion -->
+
+<!-- main footer -->
+<footer class="main-footer">
+<img src="images/skyline-banner.png" alt="skyline banner">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <ul class="footer-list">
+          <li><a href="#">Quiénes somos</a></li>
+          <li><a href="#">Contacto</a></li>
+          <li><a href="#">Trabaje con nosotros</a></li>
+          <br>
+          <br>
+        </ul>
+      </div>
+      <div class="col-12 col-md-4">
+        <ul class="footer-list">
+          <li><a href="form-login.html">Inicio</a></li>
+          <li><a href="profile.html">Mi perfil</a></li>
+          <li><a href="faq.php">Preguntas frecuentes</a></li>
+          <br>
+          <br>
+        </ul>
+      </div>
+      <div class="col-12 col-md-4">
+        <ul class="footer-list">
+          <li><a href="#">Eventos</a></li>
+          <li><a href="#">Destinos</a></li>
+          <br>
+          <br>
+          <br>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-8 copyright">
+        <img class="iso" src="images/wander-iso.png" alt="wander isotype"/>
+        <p>© derechos reservados</p>
+      </div>
+      <div class="col-4 social-links">
+        <a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+        <a href="https://twitter.com/"><i class="fab fa-twitter-square"></i></a>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- /main footer -->
+</body>
+</html>
