b15c44be34e24aaf6fa7a011084988f93be871b1
Mayra1988 <mayra.soruco@hotmail.com>
2019-05-11 19:03:05 -0300

    formulario de registro asociado a styles.css y con head con php

--- register.php
+++ register.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<?php
+  $docTitle = "Wander - Registro";
+
+  require_once("partials/head.php");
+ ?>
+
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registro</title>
-    <link rel="stylesheet" href="css/styles-registration.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
   </head>
 	<body>
@@ -16,7 +15,7 @@
 		<!-- /main-header -->
 
 		<!-- formulario -->
-    <div class="fondo">
+    <div class="register-body">
       <div class="titulo">
         <h1>Registro</h1>
       </div>
