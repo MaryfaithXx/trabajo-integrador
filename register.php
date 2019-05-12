ddcc5902b7269e6d717b4a3f4d073c06c6f7e094
Mayra1988 <mayra.soruco@hotmail.com>
2019-05-12 16:22:15 -0300

    actualizacion en login.php y register.php y su css de estilos

--- register.php
+++ register.php
@@ -7,7 +7,21 @@
 		<?php
 			require_once("partials/nav-bar.php");
 		?>
-		<!-- /main-header -->
+
+  <?php
+  $countries = [
+    'ar' => 'Argentina',
+    'bo' => 'Bolivia',
+    'br' => 'Brasil',
+    'co' => 'Colombia',
+    'cl' => 'Chile',
+    'ec' => 'Ecuador',
+    'pa' => 'Paraguay',
+    'pe' => 'Perú',
+    'uy' => 'Uruguay',
+    've' => 'Venezuela',
+  ];
+   ?>
 
 		<!-- formulario -->
     <div class="register-body">
@@ -17,32 +31,56 @@
       <div class="formulario">
         <form>
           <div class="form-row">
-            <div class="form-group col-md-6">
-            <!--<label >Nombre</label>-->
+            <div class="form-group col-md-3">
+            <!--<label >Nombre completo</label>-->
               <input type="text" class="form-control" placeholder="Nombre y apellido">
             </div>
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-3">
+            <!--<label >Nombre usuario</label>-->
+              <input type="text" class="form-control" placeholder="Nombre de usuario">
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-3">
+              <select name="country" class="form-control">
+                <option value="">Elegí un país</option>
+                <option value="">Argentina</option>
+                <option value="">Bolivia</option>
+                <option value="">Brasil</option>
+                <option value="">Colombia</option>
+                <option value="">Chile</option>
+                <option value="">Ecuador</option>
+                <option value="">Perú</option>
+                <option value="">Uruguay</option>
+                <option value="">Paraguay</option>
+                <option value="">Venezuela</option>
+              </select>
+            </div>
+            <div class="form-group col-md-3">
             <!--<label for="inputEmail4">Email</label>-->
               <input type="email" class="form-control" id="inputEmail4" placeholder="E-mail">
             </div>
           </div>
           <div class="form-row">
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-3">
             <!--<label for="inputPassword4">Password</label>-->
             <input type="password" class="form-control" id="inputPassword4" placeholder="Contraseña">
           </div>
-            <div class="form-group col-md-6">
+            <div class="form-group col-md-3">
             <!--<label for="inputPassword4">Password</label>-->
             <input type="password" class="form-control" id="inputPassword4" placeholder="Confirme contraseña">
             </div>
           </div>
-          <div class="final-form">
-            <div class="form-group">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">
-                Recordarme
-              </label>
+          <div class="form-row">
+            <div class="form-group col-md-3">
+              <!--<label>Imagen de perfil</label>-->
+              <div class="custom-file">
+                <input
+                  type="file"
+                  name="avatar"
+                  class="custom-file-input <?= isset($errorsInRegister['avatar']) ? 'is-invalid' : null; ?>"
+                >
+                <label class="custom-file-label">Subi tu foto de perfil...</label>
               </div>
             </div>
           </div>
