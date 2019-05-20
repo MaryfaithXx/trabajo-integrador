bd716eae7fe4bb99aa905cedeacaf256918d4be3
MaryfaithXx <mariafe_gonzalez@hotmail.com>
2019-05-20 18:01:36 -0300

    actualizacion de nav-bar y styles - creacion de logout.php

--- partials/nav-bar.php
+++ partials/nav-bar.php
@@ -1,4 +1,5 @@
 <!-- main-header -->
+	<?php require_once("register-login-controller.php"); ?>
 <header class="main-header">
 	<div class="container-fluid">
 		<div class="row">
@@ -6,12 +7,28 @@
 				<img class="iso" src="images/wander-iso.png" alt="wander isotype"/>
 				<a href= "index.php"><img class="logo" src="images/wander-logo.png" alt="wander logotype"/></a>
 			</div>
+			<!-- Verificar si el usuario NO está logueado para mostrar los enlaces Register y Login -->
+			<?php if ( !isLogged() ) : ?>
 			<div class="col-2">
 				<a href="register.php" class="register-link">Registrarme</a>
 			</div>
 			<div class="col-2">
 				<a id="login-button" href="#">Iniciar sesion</a>
 			</div>
+			<?php else: ?>
+			<div class="col-2">
+				<img class="nav-avatar" src="data/avatars/<?= $_SESSION['userLoged']['avatar']; ?>">
+			</div>
+			<div class="col-2">
+				<div class="user-menu">
+					<a class="dropdown-button"><?= $_SESSION['userLoged']['name'] ?> <i class="fa fa-caret-down"></i></a>
+						<div class="dropdown-content">
+						  <a href="profile.php">Mi perfil</a>
+						  <a href="logout.php">Salir</a>
+						</div>		
+				</div>
+			</div>
+			<?php endif ?>
 		</div>
 	</div>
 	<?php require_once("login.php"); ?>
